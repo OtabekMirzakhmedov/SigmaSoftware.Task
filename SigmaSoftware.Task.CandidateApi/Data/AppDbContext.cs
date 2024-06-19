287b03a6dcb63cd5ed3c04a53c947bd19a20a531
otabek.mirzakhmedov <otabek.mirzakhmedov@outlook.com>
2024-06-19 12:01:54 +0500

    adding DbContext

---
+++ SigmaSoftware.Task.CandidateApi/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SigmaSoftware.Task.CandidateApi.Models;
+
+namespace SigmaSoftware.Task.CandidateApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Candidate> Candidates { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Candidate>()
+                .HasIndex(c => c.Email)
+                .IsUnique();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
