e749b714f0aef1b36c33affcdbb66ef7a92aecaf
otabek.mirzakhmedov <otabek.mirzakhmedov@outlook.com>
2024-06-19 18:01:48 +0500

    deleting default weather forecast

--- SigmaSoftware.Task.CandidateApi/Program.cs
+++ SigmaSoftware.Task.CandidateApi/Program.cs
@@ -5,22 +5,17 @@ using SigmaSoftwareTask.CandidateApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("TaskConnection")));
 
 builder.Services.AddScoped<ICandidateService, CandidateService>();
 builder.Services.AddScoped<ICandidateMapper, CandidateMapper>();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
