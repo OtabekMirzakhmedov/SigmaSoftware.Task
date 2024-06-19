cf29cad939a99f1ba216e9083fe0308810feb106
otabek.mirzakhmedov <otabek.mirzakhmedov@outlook.com>
2024-06-19 15:17:59 +0500

    adding create, update, read candidate services

---
+++ SigmaSoftware.Task.CandidateApi/Services/CandidateService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SigmaSoftwareTask.CandidateApi.Data;
+using SigmaSoftwareTask.CandidateApi.Models;
+
+namespace SigmaSoftwareTask.CandidateApi.Services
+{
+    public class CandidateService: ICandidateService
+    {
+        private readonly AppDbContext _context;
+
+        public CandidateService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddOrUpdateCandidateAsync(Candidate candidate)
+        {
+            var existingCandidate = await _context.Candidates
+                .FirstOrDefaultAsync(c => c.Email == candidate.Email);
+
+            if (existingCandidate != null)
+            {
+                existingCandidate.FirstName = candidate.FirstName;
+                existingCandidate.LastName = candidate.LastName;
+                existingCandidate.PhoneNumber = candidate.PhoneNumber;
+                existingCandidate.CallTimeInterval = candidate.CallTimeInterval;
+                existingCandidate.LinkedInUrl = candidate.LinkedInUrl;
+                existingCandidate.GitHubUrl = candidate.GitHubUrl;
+                existingCandidate.FreeTextComment = candidate.FreeTextComment;
+
+                _context.Candidates.Update(existingCandidate);
+            }
+            else
+            {
+                _context.Candidates.Add(candidate);
+            }
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Candidate>> GetAllCandidatesAsync()
+        {
+            return await _context.Candidates.ToListAsync();
+        }
+    }
+}
---
+++ SigmaSoftware.Task.CandidateApi/Services/ICandidateService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SigmaSoftwareTask.CandidateApi.Models;
+
+namespace SigmaSoftwareTask.CandidateApi.Services
+{
+    public interface ICandidateService
+    {
+        Task AddOrUpdateCandidateAsync(Candidate candidate);
+        Task<IEnumerable<Candidate>> GetAllCandidatesAsync();
+    }
+}
